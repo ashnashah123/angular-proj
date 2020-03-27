bbba10e72b1826cafb802568bdb05d7452ca5c5a
Ashna Shah <shah.ash@husky.neu.edu>
2020-03-27 15:56:14 -0400

    fixing heroku deploy issues

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/src'));
+app.use(express.static(__dirname + '/dist/angular-proj/src'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/src/index.html'));});
+'/dist/angular-proj/src/index.html'));});
 app.listen(process.env.PORT || 8080);
