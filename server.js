7f0da21f2d3426360302d50560e95039a47d5e4a
Ashna Shah <shah.ash@husky.neu.edu>
2020-03-29 17:31:52 -0400

    server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/angular-proj'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/angular-proj/src/index.html'));});
+'/dist/angular-proj/index.html'));});
 app.listen(process.env.PORT || 8080);
