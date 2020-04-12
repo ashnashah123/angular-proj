25db85e4f0e50174b0a81df9ed49b38eb3dad410
Ashna Shah <shah.ash@husky.neu.edu>
2020-04-12 17:00:34 -0400

    heroku stuff

--- src/app/services/question.service.client.ts
+++ src/app/services/question.service.client.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuestionServiceClient {
   findQuestionsForQuiz = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}/questions`)
+    // fetch(`http://localhost:3000/api/quizzes/${qid}/questions`)
+    fetch(`https://sleepy-tundra-69887.herokuapp.com/api/quizzes/${qid}/questions`)
       .then(response => response.json())
 }
--- src/app/services/quiz.service.client.ts
+++ src/app/services/quiz.service.client.ts
@@ -2,9 +2,11 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuizServiceClient {
   findAllQuizzes = () =>
-    fetch('http://localhost:3000/api/quizzes')
+    // fetch('http://localhost:3000/api/quizzes')
+    fetch('https://sleepy-tundra-69887.herokuapp.com/api/quizzes')
       .then(response => response.json())
   findQuizById = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}`)
+    // fetch(`http://localhost:3000/api/quizzes/${qid}`)
+    fetch(`https://sleepy-tundra-69887.herokuapp.com/api/quizzes/${qid}`)
       .then(response => response.json())
 }
