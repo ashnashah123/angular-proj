715a9bdfdf1433108b1eb5466dbc6cafa5a654ea
Ashna Shah <shah.ash@husky.neu.edu>
2020-04-12 17:08:23 -0400

    update heroku stuff

--- src/app/services/question.service.client.ts
+++ src/app/services/question.service.client.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuestionServiceClient {
   findQuestionsForQuiz = (qid) =>
-    // fetch(`http://localhost:3000/api/quizzes/${qid}/questions`)
-    fetch(`https://sleepy-tundra-69887.herokuapp.com/api/quizzes/${qid}/questions`)
+    fetch(`https://guarded-anchorage-75909.herokuapp.com/api/quizzes/${qid}/questions`)
       .then(response => response.json())
 }
