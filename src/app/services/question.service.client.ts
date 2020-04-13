717bb0fbd2f4ad589805cc9e05e53d689183898d
Ashna Shah <shah.ash@husky.neu.edu>
2020-04-13 00:09:15 -0400

    heroku stuff

--- src/app/services/question.service.client.ts
+++ src/app/services/question.service.client.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuestionServiceClient {
   findQuestionsForQuiz = (qid) =>
-    fetch(`https://guarded-anchorage-75909.herokuapp.com/api/quizzes/${qid}/questions`)
+    // fetch(`http://localhost:3000/api/quizzes/${qid}/questions`)
+    fetch(`https://secure-tundra-22276.herokuapp.com/api/quizzes/${qid}/questions`)
       .then(response => response.json())
 }
--- src/app/services/quiz.service.client.ts
+++ src/app/services/quiz.service.client.ts
@@ -2,9 +2,11 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuizServiceClient {
   findAllQuizzes = () =>
-    fetch('https://guarded-anchorage-75909.herokuapp.com/api/quizzes')
+    // fetch('http://localhost:3000/api/quizzes')
+    fetch('https://secure-tundra-22276.herokuapp.com/api/quizzes')
       .then(response => response.json())
   findQuizById = (qid) =>
-    fetch(`https://guarded-anchorage-75909.herokuapp.com/api/quizzes/${qid}`)
+    // fetch(`http://localhost:3000/api/quizzes/${qid}`)
+    fetch(`https://secure-tundra-22276.herokuapp.com/api/quizzes/${qid}`)
       .then(response => response.json())
 }
